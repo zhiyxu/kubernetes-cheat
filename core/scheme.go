6197cd34c79a3ed0d8ca92f53901c0f3d1dcc429
zhiyxu <xzy_1108@163.com>
2020-10-14 09:48:56 +0800

    Optimize
    Signed-off-by: zhiyxu <xzy_1108@163.com>

--- core/scheme.go
+++ core/scheme.go
@@ -46,19 +46,27 @@ func main() {
 
 	// usage
 	// 通过GV查Type
+	// map[Pod:v1.Pod Status:v1.Status]
 	fmt.Println(scheme.KnownTypes(coreGV))
 	// 返回gvkToType数据结构
+	// map[/__internal, Kind=Pod:v1.Pod /v1, Kind=Pod:v1.Pod /v1, Kind=Status:v1.Status
+	// extensions/v1beta1, Kind=DaemonSet:v1.DaemonSet]
 	fmt.Println(scheme.AllKnownTypes())
 	// 通过Object查询GVK
+	// [/v1, Kind=Pod /__internal, Kind=Pod] false <nil>
 	fmt.Println(scheme.ObjectKinds(&corev1.Pod{})) // pod的指针类型才实现了runtime.Object
 	// 返回某个GVK的对象实例
 	fmt.Println(scheme.New(coreGV.WithKind("Pod")))
 	// Group是否注册
+	// true
 	fmt.Println(scheme.IsGroupRegistered(""))
 	// GV是否注册
+	// true
 	fmt.Println(scheme.IsVersionRegistered(coreGV))
 	// GVK是否已经注册
+	// true
 	fmt.Println(scheme.Recognizes(coreGV.WithKind("Pod")))
 	// 是否属于Unversioned
+	// true true
 	fmt.Println(scheme.IsUnversioned(&metav1.Status{}))
 }
