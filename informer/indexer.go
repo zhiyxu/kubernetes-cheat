cfad7e0e8c4e5fda306755b513ceb2269b64184b
zhiyxu <xzy_1108@163.com>
2020-11-25 14:39:58 +0800

    Update Indexer
    Signed-off-by: zhiyxu <xzy_1108@163.com>

--- informer/indexer.go
+++ informer/indexer.go
@@ -40,25 +40,27 @@ func main() {
 	}
 
 	/*
-	threadSafeMap数据结构：
+	cache.cacheStorage/threadSafeMap数据结构包含了indexers、indices、items：
 
 	indexers = map[string]IndexFunc {"byUser": UsersIndexFunc}
 	(初始化时传入的参数)
 
-	indices = map["byUser"]index
-	其中index =
-		map[string]sets.String {
-			"ernie": "default/one", "default/tre"
-			"bert": "default/one", "default/two",
-			"oscar": "default/two",
-			"elmo": "default/tre"
+	indices = map["byUser"]cache.Index
+	其中
+	cache.Index = map[string]sets.String {
+			"ernie": "one", "tre"
+			"bert": "one", "two",
+			"oscar": "two",
+			"elmo": "tre"
 		}
+	这里的indice表示在"byUser"这个indexer的索引下，每个索引对应着哪些对象，其中对象的名称是由KeyFunc计算出来的
 
-	items = map[string]interface {
-		"default/one": pod1,
-		"default/two": pod2,
-		"default/tre": pod3,
+	items = map[string]interface{} {
+		"one": corev1.pod1,
+		"two": corev1.pod2,
+		"tre": corev1.pod3,
 	}
+	items中根据名称存储着所有的对象，其中的key是由KeyFunc计算出来的，val就是对象的完整结构
 	 */
 	index.Add(pod1)
 	index.Add(pod2)
